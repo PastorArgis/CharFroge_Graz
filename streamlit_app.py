fab388e33d9c61c0f306746149ee2109215588d9
Albert Hirtenfelder <albert.hirtenfelder@student.tugraz.at>
2026-03-29 18:28:50 +0200

    Test App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+
+st.title("DnD Character Sheet")
+
+name = st.text_input("Name", "Arannis")
+level = st.number_input("Level", 1, 20, 3)
+
+st.write(f"{name} ist Level {level}")
